689b70d3286460f8c5ee55e7bfa31cd3f8c732b8
pramodsawant <39677907+pramodsawant@users.noreply.github.com>
2019-02-19 13:15:23 +0530

    added more comment

--- src/main/java/com/telusko/RestfullREST/model/Employee.java
+++ src/main/java/com/telusko/RestfullREST/model/Employee.java
@@ -1,5 +1,5 @@
 package com.telusko.RestfullREST.model;
-//added pramod
+//added pramod sawant
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
