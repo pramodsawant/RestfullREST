8ed1d7a407b3692c7b880ef0592eeb4b35643a57
pramsawa <pramsawa@LIN16008169.corp.capgemini.com>
2019-02-27 20:23:59 +0530

    record and index changed

--- src/main/java/com/telusko/RestfullREST/dao/EmployeeDAO.java
+++ src/main/java/com/telusko/RestfullREST/dao/EmployeeDAO.java
@@ -27,6 +27,7 @@ public class EmployeeDAO {
 		Employee employee5 = new Employee("E05", "atul", "nashik");
 		Employee employee6 = new Employee("E06", "asha", "old nsk");
 		Employee employee7 = new Employee("E07", "jaya", "cidco");
+		Employee employee8 = new Employee("E08", "aasha", "nashik");
 		empMap.put(employee1.getEmpno(), employee1);
 		empMap.put(employee2.getEmpno(), employee2);
 		empMap.put(employee3.getEmpno(), employee3);
@@ -36,6 +37,7 @@ public class EmployeeDAO {
 		empMap.put(employee5.getEmpno(), employee5);
 		empMap.put(employee6.getEmpno(), employee6);
 		empMap.put(employee7.getEmpno(), employee7);
+		empMap.put(employee8.getEmpno(), employee8);
 	}
 	
 	public static Employee getemp(String empno)
